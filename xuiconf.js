5a5caf3ab955edaf217ef431daf59f08d78e29bf
tariqb15 <56489426+tariqb15@users.noreply.github.com>
2019-11-23 11:24:52 -0600

    Updated by CrossUI No-Code App Builder

--- xuiconf.js
+++ xuiconf.js
@@ -1,1 +1,22 @@
 //
+
+// [[Page Appearance
+xui.ini.$PageAppearance = {
+    "theme":"default"
+};
+// ]]Page Appearance
+
+
+// [[Font Icons CDN
+
+// ]]Font Icons CDN
+
+
+// [[Develop Env Setting
+xui.ini.$DevEnv = {
+    "designViewConf":{
+        "width":800,
+        "height":600
+    }
+};
+// ]]Develop Env Setting
